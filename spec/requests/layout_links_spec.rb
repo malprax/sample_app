c5e35162e61806e17cd98348d41efbde0a903921
aulia sabril <kingmalprax@gmail.com>
2013-02-13 23:02:24 +0800

    Pas Baru Buat Halaman Sign Up

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -23,6 +23,11 @@ describe "LayoutLinks" do
 	response.should have_selector('title', :content => "Help")
 	end
 
+	it "should have a signup page at '/signup'" do
+	get '/signup'
+	response.should have_selector('title', :content => "Sign up")
+	end
+
     #it "works! (now write some real specs)" do
     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     # get layout_links_index_path
